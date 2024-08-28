51bf437a5c9a780987458f52844cec1d48a8485c
egoRockU <lagutan.gerwin.bscs2021@gmail.com>
2024-08-28 11:06:28 +0800

    bug removed

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import CameraDetect from './components/CameraDetect';
+import { StyleSheet, View } from 'react-native';
 import CameraSnap from './components/CameraSnap';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      {/* <CameraDetect /> */}
       <CameraSnap />
       <StatusBar style="auto" />
     </View>
